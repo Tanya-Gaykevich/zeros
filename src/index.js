742b460f72a1ac77373c6fccca36e0b51159241f
Tanya-Gaykevich <55203270+Tanya-Gaykevich@users.noreply.github.com>
2019-10-12 23:54:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+	var multipliers = { '2': 0, '5': 0 };
+
+	expression.split('*').forEach(fact => {
+
+		var isDouble = fact.match(/!/g).length == 2 ? true : false;
+		fact = fact.substring(0, fact.indexOf('!'))
+		var factorialParity = fact % 2 == 0 ? true : false;
+
+		Object.keys(multipliers).forEach(m => {
+			if (isDouble && !factorialParity && m % 2 == 0) return;
+			var limit = 1;
+			do {
+				limit *= m;
+				var count = Math.floor(fact / limit);
+				if (isDouble && m % 2 != 0) {
+					if (factorialParity) count = Math.floor(count / 2);
+					else count = Math.ceil(count / 2);
+				}
+				multipliers[m] += count;
+			} while (limit <= fact)
+		})
+	})
+	return Math.min(multipliers['2'], multipliers['5'])
 }
